51770c4f4f4d716cf2191d39881395425498c3da
Abdul Wahid <wahdiahmed890@gmail.com>
2022-03-17 18:53:02 +0600

    add

---
+++ hellos.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+
+    int a, b, c;
+    scanf("%d %d %d", &a, &b, &c);
+    int odds = 0;
+
+    if (a & 1)
+        odds++;
+
+    if (b & 1)
+        odds++;
+
+    if (c & 1)
+        odds++;
+
+    printf("%s\n", odds > 0 and odds < 3 ? "3 Kimonos for Nezuko" : "You have to choose two");
+    return 0;
+}
